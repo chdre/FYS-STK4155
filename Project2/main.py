102516170cc7fe233739c757fe5f861762314707
chdre <christerdr@outlook.com>
2019-10-22 12:52:07 +0200

    Finished function to import and transform data

--- Project2/main.py
+++ Project2/main.py
@@ -7,23 +7,21 @@ import seaborn as sns
 import tensorflow as tf
 import pandas as pd
 import os
-from tabulate import tabulate
 
 
 np.random.seed(0)
 
 
-def import_data():
+def data_import():
+    """
+    Imports credit card data, returns test and train set for design matrix and y.
+    """
+    # Importing data
     cwd = os.getcwd()   # Current working directory
     filename = cwd + '/data/default of credit card clients.xls'
     nanDict = {}    # To store NaN from CC data when reading with pandas?
     df = pd.read_excel(filename, header=1, index_col=0,
                        skiprows=0, na_values=nanDict)  # Dataframe
-
-    # Renaming axis
-    df.rename(index=str,
-              columns={df.columns[-1]: 'DefaultPaymentNextMonth'},
-              inplace=True)
 
     # Dropping rows where no billing and no payment
     df = df.drop(df[(df.BILL_AMT1 == 0) &
@@ -39,22 +37,27 @@ def import_data():
                     (df.PAY_AMT5 == 0) &
                     (df.PAY_AMT6 == 0)].index)
 
+    # Renaming axis
+    df.rename(index=str,
+              columns={df.columns[-1]: 'DefaultPaymentNextMonth'},
+              inplace=True)
     # Features and targets
     x = df.loc[:, df.columns != 'DefaultPaymentNextMonth'].values
     y = df.loc[:, df.columns == 'DefaultPaymentNextMonth'].values
 
-    # Design matrix
-    X = np.c_[np.array([1] * len(x[:, 0])), X]
-
     # Train-test split
     train_size_ = 0.8
-    Xtest, Xtrain, ytest, ytrain = train_test_split(
-        X, y, train_size=train_size_, test_size=1 - train_size_)
+    xtest, xtrain, ytest, ytrain = train_test_split(
+        x, y, train_size=train_size_, test_size=1 - train_size_)
 
     # Scaling
     scale = StandardScaler()   # Scales by (func - mean)/std.dev
-    Xtrain = scale.fit_transform(Xtrain)
-    Xtest = scale.transform(Xtest)
+    xtrain = scale.fit_transform(xtrain)
+    xtest = scale.transform(xtest)
+
+    # Design matrix
+    Xtrain = np.c_[np.array([1] * len(xtrain[:, 0])), xtrain]
+    Xtest = np.c_[np.array([1] * len(xtest[:, 0])), xtest]
 
     return Xtrain, Xtest, ytrain, ytest
 
@@ -73,26 +76,27 @@ def stochastic_gradient_descent(X, y, beta,
 
     if mini_batch:
         for epoch in range(n_epochs):
-            while np.abs(beta_old - beta_new) < eps or iter < n:
-                rand_idx1 = np.random.randint(int(n / 2))
-                rand_idx2 = np.random.randint(int(n / 2))
-                xi = X[rand_idx1:rand_idx + 1,
-                       rand_idx2:rand_idx2 + 1]  # Matrix
-                yi = y[rand_idx1:rand_idx1 + 1]  # Array
-                P = np.exp(X @ beta) / (1 + np.exp(X @ beta))
-                gradient = X.T @ (P - y)
-                eta = learning_schedule(epoch * m + i)
-                beta -= eta * gradient
+            if np.abs(beta_old - beta_new) < eps:
+                while np.abs(beta_old - beta_new) < eps or iter < n:
+                    rand_idx1 = np.random.randint(int(n / 2))
+                    rand_idx2 = np.random.randint(int(n / 2))
+                    xi = X[rand_idx1:rand_idx + 1,
+                           rand_idx2:rand_idx2 + 1]  # Matrix
+                    yi = y[rand_idx1:rand_idx1 + 1]  # Array
+                    P = np.exp(X @ beta) / (1 + np.exp(X @ beta))
+                    gradient = X.T @ (P - y)
+                    eta = learning_schedule(epoch * m + i)
+                    beta -= eta * gradient
 
-                beta_old = beta
-                beta = beta_new
+                    beta_old = beta
+                    beta = beta_new
 
-        return beta_new
+            return beta_new
 
     if not mini_batch:
         for i in range(n):
-            P = np.exp(X @ beta) / (1 + np.exp(X @ beta))
-            gradient = X.T  @ (P - y)
+            P = np.exp(X @ beta) / (1 + np.exp(X @ beta))
+            gradient = X.T @ (P - y)
             beta_new = beta - eta * gradient
 
             if abs(np.sum(beta - beta_new)) < eps:
@@ -106,7 +110,10 @@ def stochastic_gradient_descent(X, y, beta,
 
 def main():
 
-    beta = np.random.randn(len(x[:, 0]), 1)
+    Xtrain, Xtest, ytrain, ytest = import_data()
+    beta = np.random.randn(len(Xtrain[0]), 1)
+
+    beta = stochastic_gradient_descent(Xtrain, ytrain, beta, mini_batch=False)
 
 
 if __name__ == '__main__':
