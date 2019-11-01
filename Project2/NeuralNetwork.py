e02d7e81382b0a13ccb59239827ec37efad03602
chdre <christerdr@outlook.com>
2019-11-01 17:18:05 +0100

    Works with digits

--- Project2/NeuralNetwork.py
+++ Project2/NeuralNetwork.py
@@ -17,7 +17,6 @@ class NeuralNetwork:
             batch_size=100):
         self.x_data_full = x_data
         self.y_data_full = y_data
-        np.random.shuffle(self.y_data_full)
 
         self.sizes = sizes
         self.n_inputs = x_data.shape[0]
@@ -35,78 +34,97 @@ class NeuralNetwork:
         self.create_bias_and_weights()
 
     def create_bias_and_weights(self):
-        self.biases = np.asarray([np.zeros(y) + 0.01 for y in self.sizes[1:]])
-        self.weights = np.asarray([np.random.randn(x, y) for x, y in zip(
+        self.b = np.asarray([np.zeros(y) + 0.01 for y in self.sizes[1:]])
+        self.w = np.asarray([np.random.randn(x, y) for x, y in zip(
             self.sizes[:-1], self.sizes[1:])])
 
-    def backpropagation(self):
-        """ backprop"""
-        error = np.empty(self.layers, dtype=np.ndarray)
-        self.weights_gradient = np.empty(len(self.weights), dtype=np.ndarray)
-        self.biases_gradient = np.empty(len(self.biases), dtype=np.ndarray)
-
-        error[-1] = self.probabilities - self.y_data
-
-        self.weights_gradient[-1] = np.matmul(self.a[-1].T, error[-1])
-        self.biases_gradient[-1] = np.sum(error[-1], axis=0)
-        grad1 = self.biases_gradient[-1]
-
-        for l in range(1, self.layers):
-            error[-l - 1] = np.matmul(error[-l], self.weights[-l].T) * \
-                self.a[-l - 1] * (1 - self.a[-l - 1])
-
-            self.weights_gradient[-l] = np.matmul(self.a[-l - 1].T, error[-l])
-            self.biases_gradient[-l] = np.sum(error[-l], axis=0)
-
-            if self.lmbda > 0:
-                self.weights_gradient[-l] += self.lmbda * self.weights[-l]
-
-            self.weights[-l] -= self.eta * self.weights_gradient[-l]
-            self.biases[-l] -= self.eta * self.biases_gradient[-l]
+    def set_activation_function(self, i):
+        if isinstance(self.activation_func, (list, tuple, np.ndarray)):
+            self.act_func = self.activation_func[i]
+        elif isinstance(self.activation_func, str):
+            self.act_func = self.activation_func
 
     def feed_forward(self):
         """
         Performs a feed forward, storing activation and z
         """
-        self.z = np.empty(len(self.biases), dtype=np.ndarray)
-        self.a = np.empty(len(self.biases) + 1, dtype=np.ndarray)
+        self.z = np.empty(len(self.b), dtype=np.ndarray)
+        self.a = np.empty(len(self.b) + 1, dtype=np.ndarray)
         self.a[0] = self.x_data
 
-        for i in range(len(self.biases)):
-            if isinstance(self.activation_func, (list, tuple, np.ndarray)):
-                self.activation_function = self.activation_func[l]
-            elif isinstance(self.activation_func, str):
-                self.activation_function = self.activation_func
-            z = np.matmul(self.a[i], self.weights[i]) + self.biases[i]
+        for i in range(self.b.shape[0]):
+            self.set_activation_function(i)
+            z = np.matmul(self.a[i], self.w[i]) + self.b[i]
             self.z[i] = z
-            self.a[i + 1] = self.forward_activation(
-                z) if i < len(self.biases) - 1 else softmax(z)
-        """
-        Lager aldri z av siste a
-        """
+            self.a[i + 1] = self.forward_activation(z)
 
-        self.probabilities = softmax(z)
+        self.probabilities = self.a[-1]
+
+    def backpropagation(self):
+        """ backprop"""
+        error = np.empty(self.layers, dtype=np.ndarray)
+        self.w_grad = np.empty(len(self.w), dtype=np.ndarray)
+        self.b_grad = np.empty(len(self.b), dtype=np.ndarray)
+
+        error[-1] = self.probabilities - self.y_data
+
+        self.w_grad[-1] = np.matmul(self.a[-1].T, error[-1])
+        self.b_grad[-1] = np.sum(error[-1], axis=0)
+
+        for l in range(1, self.layers):
+            self.set_activation_function(-l)
+            error[-l - 1] = np.matmul(error[-l], self.w[-l].T) * \
+                self.cost_derivative(self.a[-l - 1])
+
+            self.w_grad[-l] = np.matmul(self.a[-l - 1].T, error[-l])
+            self.b_grad[-l] = np.sum(error[-l], axis=0)
+
+            if self.lmbda > 0:
+                self.w_grad[-l] += self.lmbda * self.w[-l]
+
+            self.w[-l] -= self.eta * self.w_grad[-l]
+            self.b[-l] -= self.eta * self.b_grad[-l]
 
     def forward_activation(self, x):
-        if self.activation_function == 'sigmoid':
+        if self.act_func == 'sigmoid':
+            # print("sig")
             return 1 / (1 + np.exp(-x))
-        elif self.activation_function == 'tanh':
+        elif self.act_func == 'tanh':
             return np.tanh(x)
-        elif self.activation_function == 'relu':
+        elif self.act_func == 'relu':
             return np.maximum(0, x)
-        elif self.activation_function == 'leaky_relu':
+        elif self.act_func == 'leaky_relu':
             return np.maximum(self.leaky_slope * x, x)
+        elif self.act_func == 'softmax':
+            # print("soft")
+            exp_term = np.exp(x)
+            return exp_term / np.sum(exp_term, axis=1, keepdims=True)
+
+    def cost_derivative(self, a):
+        if self.act_func == 'sigmoid':
+            return a * (1 - a)
+        elif self.act_func == 'tanh':
+            return 1 - np.square(a)
+        elif self.act_func == 'relu':
+            return 1 * (a > 0)
+        elif self.act_func == 'leaky_relu':
+            d = np.zeros_like(a)
+            d[a <= 0] = self.leaky_slope
+            d[a > 0] = 1
+            return d
+        elif self.act_func == 'softmax':
+            return a
 
     def predict(self, x):
-        z = np.empty(len(self.biases), dtype=np.ndarray)
-        a = np.empty(len(self.biases) + 1, dtype=np.ndarray)
+        z = np.empty(len(self.b), dtype=np.ndarray)
+        a = np.empty(len(self.b) + 1, dtype=np.ndarray)
         a[0] = x
 
-        for i in range(len(self.biases)):
-            _z = np.matmul(a[i], self.weights[i]) + self.biases[i]
+        for i in range(len(self.b)):
+            self.set_activation_function(i)
+            _z = np.matmul(a[i], self.w[i]) + self.b[i]
             z[i] = _z
-            a[i + 1] = self.forward_activation(_z) if i < len(
-                self.biases) - 1 else softmax(_z)
+            a[i + 1] = self.forward_activation(_z)
 
         exp_term = np.exp(z[-1])
         probabilities = np.argmax(
